33d865d88a6ad1227cd8c37961fa86ccc2ec8732
nassor <nassor@gmail.com>
2025-07-09 10:15:42 -0400

    using cbor for binary encoding/decoding

--- envelope.go
+++ envelope.go
@@ -1,13 +1,25 @@
 package envelope
 
 import (
-	"bytes"
-	"encoding/gob"
 	"hash"
 	"time"
 
+	"github.com/fxamacker/cbor/v2"
 	"golang.org/x/crypto/sha3"
 )
+
+var cborEnc cbor.EncMode
+
+func init() {
+	opts := cbor.CoreDetEncOptions()
+	opts.Time = cbor.TimeRFC3339Nano
+	opts.Sort = cbor.SortCTAP2
+	var err error
+	cborEnc, err = opts.EncMode()
+	if err != nil {
+		panic("failed to initialize CBOR encoding options: " + err.Error())
+	}
+}
 
 // SecurityFlags is a bitmask that defines the security attributes of an envelope.
 type SecurityFlags uint8
@@ -34,23 +46,23 @@ type Envelope struct {
 	// Data is the content of the envelope.
 	Data []byte
 	// Metadata is additional information about the envelope.
-	Metadata map[string]string
+	Metadata map[string]string `json:",omitempty"`
 	// TelemetryContext is a placeholder for telemetry information.
-	TelemetryContext map[string]string
+	TelemetryContext map[string]string `json:",omitempty"`
 	// Signature is the signature of the data.
-	Signature []byte
+	Signature []byte `json:",omitempty"`
 	// SecurityFlags define the security attributes of the envelope.
 	SecurityFlags SecurityFlags
 	// CreatedAt is the timestamp when the envelope was created.
 	CreatedAt time.Time
-	// ReceivedAt is the timestamp when the envelope was received.
-	ReceivedAt time.Time
 	// ExpiresAt is the timestamp when the envelope expires.
-	ExpiresAt time.Time
+	ExpiresAt time.Time `json:",omitzero"`
 
 	// --- Customization fields (unexported) ---
-	hmacHashFunc func() hash.Hash // Hash function used for HMAC computation
-	nonceSize    int              // Size of the nonce for AES-GCM encryption
+	// Hash function used for HMAC computation
+	hmacHashFunc func() hash.Hash `cbor:"-"`
+	// Size of the nonce for AES-GCM encryption
+	nonceSize int `cbor:"-"`
 }
 
 // New creates a new envelope with the given data and security flags.
@@ -74,26 +86,15 @@ func New(data []byte, opts ...EnvelopeOption) *Envelope {
 	return e
 }
 
-// MarshalBinary implements the encoding.BinaryMarshaler interface using gob.
+// MarshalBinary implements the encoding.BinaryMarshaler interface using cbor.
 func (e *Envelope) MarshalBinary() ([]byte, error) {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-
-	// Use a type alias to avoid recursive calls to MarshalBinary.
 	type envelopeAlias Envelope
-	if err := enc.Encode((*envelopeAlias)(e)); err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), nil
+	return cborEnc.Marshal((*envelopeAlias)(e))
 }
 
-// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface using gob.
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface using cbor.
 func (e *Envelope) UnmarshalBinary(data []byte) error {
-	buf := bytes.NewBuffer(data)
-	dec := gob.NewDecoder(buf)
-
 	// Use a type alias to avoid recursive calls to UnmarshalBinary.
 	type envelopeAlias Envelope
-	return dec.Decode((*envelopeAlias)(e))
+	return cbor.Unmarshal(data, (*envelopeAlias)(e))
 }
--- go.mod
+++ go.mod
@@ -5,8 +5,7 @@ go 1.24.4
 require golang.org/x/crypto v0.39.0
 
 require (
-	github.com/golangci/golangci-lint v1.64.8 // indirect
+	github.com/fxamacker/cbor/v2 v2.8.0
+	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 )
-
-tool github.com/golangci/golangci-lint
